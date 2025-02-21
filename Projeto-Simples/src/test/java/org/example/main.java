ff92f68a7e723c808963e66aa3418a2faa92affd
unknown <242-000124@IMEPACAD.local>
2025-02-20 21:34:54 -0300

    nova-Atualização

--- Projeto-Simples/src/test/java/org/example/main.java
+++ Projeto-Simples/src/test/java/org/example/main.java
@@ -1,7 +1,9 @@
 package org.example;
 
 public class main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Bem vindo ao projeto Simples");
+
+        System.out.println("nova funcionalidade adicionada!");
     }
 }

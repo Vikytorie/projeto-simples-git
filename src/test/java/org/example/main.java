1aa0d289544c8a846b0045097c9f83584a485298
unknown <242-000124@IMEPACAD.local>
2025-02-20 22:26:13 -0300

    novaatt

---
+++ src/test/java/org/example/main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Bem vindo ao projeto Simples");
+
+        System.out.println("nova funcionalidade adicionada!");
+        System.out.println("nova funcionalidade adicionada!");
+    }
+}
